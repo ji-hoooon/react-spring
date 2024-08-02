0f45c0372d10dcdcd76e2c32c5e1265a8e844011
ji-hoooon <ljh6384@naver.com>
2024-08-03 00:45:38 +0900

    7: useEffect와 의존성리스트 - 콜백함수와 의존성리스트를 받는 useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Sub from './Sub';
+//5)
+import{num} from './aa/Third'
+import { randomFill } from 'crypto';
 
 //3)jsx 문법
 let a:number = 10; //변수
 const c:number = 15; //상수 (불변)
 
 function App() {
+  //5) useEffect
+  const [data, setData] =useState(0); //초기값 0
+  //변경시에 실행 되는 변수
+  const [search,setSearch] = useState(0);
+
+  //다운로드
+  const down=()=>{
+    let downloadData = 5; //가정
+    setData(downloadData);
+  }
+
+  //(1) 실행시점 : App() 함수가 최초 실행될 때 (마운트될 때, 즉 렌더링할 때)
+  //: 매개변수 -> 콜백함수, 의존성리스트
+  //(2) 상태변수가 변경될 때
+  //(3) 의존리스트로 관리 가능
+  useEffect(() => {
+    
+    console.log("useEffect 실행됨") //데이터 변경없으면 한번 실행됨 -> 더하기 누를때 또 실행해야할까?
+    down(); //초기값 0 -> 5
+  //},[data]);  //data가 변경될때마다 실행 -> 상태변수가 변경될때마다 실행
+// },[]);  //최초에 한번만 실행 -> 어디에도 의존하지 않는다. -> useEffect 한번만 실행
+},[search]);  //data까지 변경
+
   //4) useState
   //let number:number=1; //상태값 아님
   const [number,setNumber] = useState(1); //상태값: React안의 Hooks라이브러리 이용해 상태화(Hooks가 관리하는 상태값)
@@ -96,10 +122,17 @@ let sample:User[]=[
 
   //jsx (1) 리턴은 하나의 태그만
   return ( //jsx (5) 리턴이 한줄일 떄와 한줄 초과일 때 괄호 유무의 차이
-    //4) useState
     <div>
+    {/* /5) useEffect */}
+    <button onClick={()=>{setSearch(Math.floor(Math.random() * 100))}}>검색하기</button>
+    <h1>데이터: {data}</h1>
+    {/* <button onClick={()=>{}}>더하기</button> */}
+    <button onClick={()=>{setData(data+1)}}>더하기</button> 
+    {/* 초기값 0 -> 5 -> 6 -> useEffect -> 5 : 배열에 의존하도록 변경 필요*/}
+
+    {/* 4) useState */}
       <div>
-      {/* 4) 상태값이 아닌 변수이기 때문에 바뀌지 않음 : 렌더링 시점=상태값 변경*/}
+      {/* 4) 상태값이 아닌 변수이기 때문에 바뀌지 않음 : 렌더링 시점=상태값 변경*/}
       <h1>숫자 : {number}</h1>
       {/* 4) onclick시 실행하도록 바인딩 */}
       <button onClick={add}>더하기</button>
---
+++ src/aa/Third.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+let num:number = 10;
+const Third = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export {num};
+//default는 하나만
+export default Third;
